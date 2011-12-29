ece991ece0754cd66d0796150929d69357515c6f
Ismail Badawi <isbadawi@gmail.com>
2011-12-29 08:30:33 -0500

    window title

--- main.c
+++ main.c
@@ -33,6 +33,7 @@ int main(int argc, char *argv[])
     SDL_Init(SDL_INIT_EVERYTHING);
     atexit(SDL_Quit);
     SDL_SetVideoMode(SCREEN_SIZE, SCREEN_SIZE, SCREEN_BPP, SDL_SWSURFACE);
+    SDL_WM_SetCaption("Memory game", NULL);
 
     Tile* grid = init_game_grid();
 
